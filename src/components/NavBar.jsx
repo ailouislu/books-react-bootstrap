166dd5b94edec0d568a5d44353aca0cd753fad43
LouisAugustine <nzlouis.com@gmail.com>
2022-02-13 14:57:41 +1300

    Delete the rental link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,6 @@ const NavBar = () => {
       <Nav className="me-auto">
         <Nav.Link href="/books">Books</Nav.Link>
         <Nav.Link href="/authors">Authors</Nav.Link>
-        <Nav.Link href="/rentals">Rentals</Nav.Link>
       </Nav>
       </Container>
     </Navbar>
